b2ddb76236a22e7191c217f4848ea388fcb97ded
Jadiker <happylondon4@gmail.com>
2022-11-12 19:17:20 -0600

    about to merge the translations together

--- translate.py
+++ translate.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 from google.cloud import translate_v2 as translate
 
 ENCODING = 'utf-8'
+# languages must be in Domain enum
 SOURCE_LANGUAGE = "en"
 TARGET_LANGUAGE = "fr"
 INPUT_FOLDER = "data"
@@ -23,7 +24,7 @@ UNTRANSLATED_CONLL_FILE_NAME = f"{TARGET_LANGUAGE}-orig-mulda.conll"
 TRANSLATED_CONLL_FILE_NAME = f"{TARGET_LANGUAGE}-trans-mulda.conll"
 UNTRANSLATED_CONLL_FILE = f"{OUTPUT_FOLDER}/{UNTRANSLATED_CONLL_FILE_NAME}"
 TRANSLATED_CONLL_FILE = f"{OUTPUT_FOLDER}/{TRANSLATED_CONLL_FILE_NAME}"
-RUN_GOOGLE = True
+RUN_GOOGLE_TRANSLATE = False
 SECRET_JSON = "./secret/high-comfort-368404-39708e023588.json"
 # how many full sentences to translate at once
 # (each full sentence is translated once for each entity in the sentence plus once for the sentence as a whole)
@@ -108,7 +109,11 @@ class Tag:
     tag_category: TagCategory
 
     def __str__(self):
-        return f"{self.tag_type}{f'-{self.tag_category}' if self.tag_type != TagType.O else ''}"
+        return self.tag_format
+
+    @property
+    def tag_format(self):
+        return f"{self.tag_type.value}{f'-{self.tag_category.value}' if self.tag_type != TagType.O else ''}"
     
 
 class Domain(Enum):
@@ -197,109 +202,192 @@ class Sentence:
                 # close bracket if the entity is at the end of the sentence
                 bracketed_sentence += END_BRACKET
             bracketed_sentence = bracketed_sentence.strip()
-            if START_BRACKET == END_BRACKET:
-                assert bracketed_sentence.count(START_BRACKET) == 2, f'bracketed sentence should have 2 "brackets": {bracketed_sentence}'
-            else:
-                assert bracketed_sentence.count(START_BRACKET) == bracketed_sentence.count(END_BRACKET) == 1, f'bracketed sentence should have one of each bracket: {bracketed_sentence}'
+            if not check_brackets(bracketed_sentence):
+                raise ValueError(f"Brackets were not done correctly in {bracketed_sentence}")
             bracketed_sentences.append(bracketed_sentence)
         assert len(bracketed_sentences) == len(self.entity_indexes)
         return bracketed_sentences
+
+    def get_entity(self, entity_indexes_index: int) -> str:
+        '''Return the entity at the given index'''
+        entity_index = self.entity_indexes[entity_indexes_index]
+        entity = ""
+        for i, word in enumerate(self.words):
+            if i == entity_index:
+                entity += word.token
+            elif i > entity_index:
+                if word.tag.tag_type != TagType.I:
+                    break
+                else:
+                    entity += f" {word.token}"
+        return entity
+
+    def get_entity_category(self, entity_indexes_index: int) -> TagCategory:
+        '''Return the entity type at the given index'''
+        entity_index = self.entity_indexes[entity_indexes_index]
+        return self.words[entity_index].tag.tag_category
+
+def check_brackets(sentence: str) -> bool:
+    if START_BRACKET == END_BRACKET:
+        return sentence.count(START_BRACKET) == 2
+    else:
+        return sentence.count(START_BRACKET) == sentence.count(END_BRACKET) == 1
 
 def list_to_generator(input_list):
     '''Convert a list to a generator'''
     for item in input_list:
         yield item
 
+def add_conll_word(file_desc, word: Word):
+    '''Add a word to the (open) file'''
+    file_desc.write(f"{word.token} _ _ {word.tag.tag_format}\n")
 
-if RUN_GOOGLE:
+def add_conll_id_line(file_desc, id_value: str, domain: Domain):
+    '''Add an ID to the (open) file'''
+    # TODO this may need fixing
+    file_desc.write(f"# id {id_value}\tdomain={domain.value}\n")
+
+if RUN_GOOGLE_TRANSLATE:
     translator = translate.Client.from_service_account_json(SECRET_JSON)
 
-with open(INPUT_FILE, 'r', encoding=ENCODING) as input_file:
-    sentences = []
-    sentence = Sentence()
-    # from https://stackoverflow.com/a/55188797/5049813
-    num_lines = sum(1 for line in input_file)
-    input_file.seek(0)
-    for line in tqdm(input_file, total=num_lines):
-        line = line.strip()
-        if not line:
-            if sentence.words:
-                sentences.append(sentence)
-            sentence = Sentence()
-        elif line.startswith("# id"):
-            # use fullmatch to get the id and domain
-            # the line should look like
-            # # id bb81b9a7-e73d-4977-b6a8-0f7937123dfe domain=en
-            # (note that the domain is separated out by a tab)
-            match = re.fullmatch(rf"# id (?P<{ID_VALUE_REGEX_KEY}>[a-zA-Z0-9-]+)\sdomain=(?P<{DOMAIN_REGEX_KEY}>[a-z]+)", line)
-            if not match:
-                raise ValueError(f"The id line is not formatted correctly: {line}")
-            assert sentence.id_value is None, "The id value has already been set"
-            sentence.id_value = match.group(ID_VALUE_REGEX_KEY)
-            sentence.domain = Domain(match.group(DOMAIN_REGEX_KEY))
-        else:
-            # use fullmatch to get the token, tag, and tag type
-            # the line should look like
-            # `tongzhi _ _ B-OtherPER`
-            # or
-            # `founder _ _ O`
-            # (spaces here are just spaces)
-            match = re.fullmatch(rf"(?P<{TOKEN_REGEX_KEY}>\S+) _ _ (?P<{TAG_TYPE_REGEX_KEY}>[BIO])(-(?P<{TAG_REGEX_KEY}>[\S]+))?", line)
-            if not match:
-                raise ValueError(f"The word line is not formatted correctly: {line}")
-            
-            token = match.group(TOKEN_REGEX_KEY)
-            tag_type = TagType(match.group(TAG_TYPE_REGEX_KEY))
-            tag_category = TagCategory(match.group(TAG_REGEX_KEY)) if tag_type != TagType.O else TagCategory.Empty
-            tag = Tag(tag_type, tag_category)
-            word = Word(token, tag)
-            sentence.add_word(word)
-
-    if sentence.words:
-        sentences.append(sentence)
-    
-    # we now have a list of sentences
-    # for each entity in each sentence, we want to put the entity in brackets and translate it
-    sentences_to_translate: List[List[str]] = [sentence.get_bracketed_sentences() for sentence in sentences]
-    if RUN_GOOGLE:
-        results = []
-        for i in tqdm(range(len(sentences_to_translate) // BATCH_SIZE + 1)):
-            start = BATCH_SIZE * i
-            end = min(BATCH_SIZE * (i + 1), len(sentences_to_translate))
-            # see https://stackoverflow.com/questions/1198777/double-iteration-in-list-comprehension
-            batch = [sentence for sentences in sentences_to_translate[start:end] for sentence in sentences]
-            results.extend(translator.translate(batch, TARGET_LANGUAGE, 'text', SOURCE_LANGUAGE))
-            break # testing
+input_file = open(INPUT_FILE, 'r', encoding=ENCODING)
+sentences = []
+sentence = Sentence()
+# from https://stackoverflow.com/a/55188797/5049813
+num_lines = sum(1 for line in input_file)
+input_file.seek(0)
+for line in tqdm(input_file, total=num_lines):
+    line = line.strip()
+    if not line:
+        if sentence.words:
+            sentences.append(sentence)
+        sentence = Sentence()
+    elif line.startswith("# id"):
+        # use fullmatch to get the id and domain
+        # the line should look like
+        # # id bb81b9a7-e73d-4977-b6a8-0f7937123dfe   domain=en
+        # (note that the domain is separated out by a tab)
+        match = re.fullmatch(rf"# id (?P<{ID_VALUE_REGEX_KEY}>[a-zA-Z0-9-]+)\sdomain=(?P<{DOMAIN_REGEX_KEY}>[a-z]+)", line)
+        if not match:
+            raise ValueError(f"The id line is not formatted correctly: {line}")
+        assert sentence.id_value is None, "The id value has already been set"
+        sentence.id_value = match.group(ID_VALUE_REGEX_KEY)
+        sentence.domain = Domain(match.group(DOMAIN_REGEX_KEY))
+    else:
+        # use fullmatch to get the token, tag, and tag type
+        # the line should look like
+        # `tongzhi _ _ B-OtherPER`
+        # or
+        # `founder _ _ O`
+        # (spaces here are just spaces)
+        match = re.fullmatch(rf"(?P<{TOKEN_REGEX_KEY}>\S+) _ _ (?P<{TAG_TYPE_REGEX_KEY}>[BIO])(-(?P<{TAG_REGEX_KEY}>[\S]+))?", line)
+        if not match:
+            raise ValueError(f"The word line is not formatted correctly: {line}")
         
-        os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-        with open(JSON_FILE, 'w', encoding=ENCODING) as f:
-            json.dump(results, f, indent=1)
-            print(f"Results saved to {JSON_FILE}")
+        token = match.group(TOKEN_REGEX_KEY)
+        tag_type = TagType(match.group(TAG_TYPE_REGEX_KEY))
+        tag_category = TagCategory(match.group(TAG_REGEX_KEY)) if tag_type != TagType.O else TagCategory.Empty
+        tag = Tag(tag_type, tag_category)
+        word = Word(token, tag)
+        sentence.add_word(word)
 
-    # we now have results in the json file
-    with open(JSON_FILE, 'r', encoding=ENCODING) as f:
-        results = json.load(f)
+if sentence.words:
+    sentences.append(sentence)
 
-    # we want to put the results back into the CONLL format
-    results_generator = list_to_generator(results)
-    with open(TRANSLATED_CONLL_FILE, 'w', encoding=ENCODING) as trans_file:
-        with open(UNTRANSLATED_CONLL_FILE_NAME, 'w', encoding=ENCODING) as orig_file:
-            for sentence_index, sentence in enumerate(sentences):
-                for entity_index in sentence.entity_indexes:
-                    result = next(results_generator)
-                    translated_sentence = result[TRANSLATED_TEXT_KEY]
-                    print("translated sentence:", translated_sentence)
+# we now have a list of sentences
+# for each entity in each sentence, we want to put the entity in brackets and translate it
+sentences_to_translate: List[List[str]] = [sentence.get_bracketed_sentences() for sentence in sentences]
+if RUN_GOOGLE_TRANSLATE:
+    results = []
+    for i in tqdm(range(len(sentences_to_translate) // BATCH_SIZE + 1)):
+        start = BATCH_SIZE * i
+        end = min(BATCH_SIZE * (i + 1), len(sentences_to_translate))
+        # see https://stackoverflow.com/questions/1198777/double-iteration-in-list-comprehension
+        batch = [sentence for sentences in sentences_to_translate[start:end] for sentence in sentences]
+        results.extend(translator.translate(batch, TARGET_LANGUAGE, 'text', SOURCE_LANGUAGE))
+        break # testing
+    
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    with open(JSON_FILE, 'w', encoding=ENCODING) as f:
+        json.dump(results, f, indent=1)
+        print(f"Results saved to {JSON_FILE}")
 
-                    # find the entity in the translated sentence
-                    # the entity should be in brackets (which may be the same character)
-                    start_bracket_find = translated_sentence.find(START_BRACKET)
-                    term_start = start_bracket_find + 1
-                    end_bracket_find = translated_sentence[term_start:].find(END_BRACKET)
-                    term_end = term_start + end_bracket_find
-                    if start_bracket_find == -1 or end_bracket_find == -1:
-                        warnings.warn(f"Could not find brackets in translated sentence: {translated_sentence}", BracketsNotFoundWarning)
-                        continue
-                    term = translated_sentence[term_start:term_end]
-                    print(term)
-                    print(term_start, term_end)
-                    raise NotImplementedError
+# we now have results in the json file
+with open(JSON_FILE, 'r', encoding=ENCODING) as f:
+    results = json.load(f)
+
+# we want to put the results back into the CONLL format
+results_generator = list_to_generator(results)
+trans_file = open(TRANSLATED_CONLL_FILE, 'w', encoding=ENCODING)
+orig_file = open(UNTRANSLATED_CONLL_FILE, 'w', encoding=ENCODING)
+for sentence_index, sentence in enumerate(sentences):
+    for entity_indexes_index, entity_index in enumerate(sentence.entity_indexes):
+        try:
+            result = next(results_generator)
+        except StopIteration:
+            raise RuntimeError(f"There wasn't one result for each entity. Was about to deal with entity {entity_indexes_index + 1} in the following sentence: {sentence}")
+        translated_sentence = result[TRANSLATED_TEXT_KEY]
+        entity_tag_category = sentence.get_entity_category(entity_indexes_index)
+        print("translated sentence:", translated_sentence)
+
+        if not check_brackets(translated_sentence):
+            warnings.warn(f"Skipping! Could not find brackets in translated sentence: {translated_sentence}", BracketsNotFoundWarning)
+            continue
+
+        # separate off symbols that come before the starting bracket and after the ending bracket
+        # for example, if there's a comma after the ending bracket
+        start_bracket_location = translated_sentence.find(START_BRACKET)
+        end_bracket_location = start_bracket_location + translated_sentence[start_bracket_location:].find(END_BRACKET)
+
+        # add a space
+        # since we're using split, extra spaces won't matter
+        translated_sentence
+
+        # add the id line
+        example_id = f"{sentence.id_value}-{entity_indexes_index}"
+        domain = Domain(TARGET_LANGUAGE)
+        add_conll_id_line(trans_file, example_id, domain)
+        add_conll_id_line(orig_file, example_id, domain)
+
+        translated_words = translated_sentence.split()
+        bracketing = False
+        for translated_word in tqdm(translated_words, desc="words", leave=False):
+            if translated_word.startswith(START_BRACKET):
+                bracketing = True
+                if translated_word.endswith(END_BRACKET):
+                    bracketing = False
+                    translated_word_without_brackets = translated_word[1:-1]
+                else:
+                    translated_word_without_brackets = translated_word[1:]
+
+                orig_entity = sentence.get_entity(entity_indexes_index)
+                orig_entity_words = orig_entity.split()
+
+                # add this one translated word to the translated file
+                word = Word(translated_word_without_brackets, Tag(TagType.B, entity_tag_category))
+                add_conll_word(trans_file, word)
+
+                # add the entire original word to the file
+                for orig_entity_word_index, orig_entity_word in enumerate(orig_entity_words):
+                    if orig_entity_word_index == 0:
+                        entity_tag_type = TagType.B
+                    else:
+                        entity_tag_type = TagType.I
+                    word = Word(orig_entity_word, Tag(entity_tag_type, entity_tag_category))
+                    add_conll_word(orig_file, word)
+
+            elif bracketing:
+                if translated_word.endswith(END_BRACKET):
+                    bracketing = False
+                    translated_word_without_brackets = translated_word[:-1]
+                word = Word(translated_word_without_brackets, Tag(TagType.I, entity_tag_category))
+                add_conll_word(trans_file, word)
+            else:
+                word = Word(translated_word, Tag(TagType.O, TagCategory.Empty))
+                add_conll_word(trans_file, word)
+                add_conll_word(orig_file, word)
+        trans_file.write("\n\n")
+        orig_file.write("\n\n")
+input_file.close()
+trans_file.close()
+orig_file.close()
